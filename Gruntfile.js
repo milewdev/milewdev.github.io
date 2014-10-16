90b056105764afe693e83590c69779ee0218b576
Michael Lewandowski <milewgit@gmail.com>
2014-10-16 19:35:52 -0400

    Add comment to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         livereload: true
       },
       html: {
-        files: ['index.html']
+        files: ['index.html']   // so that it causes a livereload
       },
       styles: {
         files: ['scss/style.scss'],
